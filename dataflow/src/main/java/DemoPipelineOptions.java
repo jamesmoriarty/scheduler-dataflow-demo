be3df5dc827f0dba22f3ee6f72c05eb6193c2997
James Moriarty <96487+jamesmoriarty@users.noreply.github.com>
2023-11-05 15:30:06 +1300

    wip

--- dataflow/src/main/java/DemoPipelineOptions.java
+++ dataflow/src/main/java/DemoPipelineOptions.java
@@ -4,8 +4,8 @@ import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.ValueProvider;
 
 public interface DemoPipelineOptions extends DataflowPipelineOptions {
-    @Description("Subscription name")
-    @Default.String("demo_subscription")
-    ValueProvider<String> getSubscription();
-    void setSubscription(ValueProvider<String> subscription);
+    // @Description("Subscription name")
+    // @Default.String("demo_subscription")
+    // ValueProvider<String> getSubscription();
+    // void setSubscription(ValueProvider<String> subscription);
 }
